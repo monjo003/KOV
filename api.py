34859b0172b8ae513cced5251f97deb2751b3d10
Abenezer Monjor <abenezermonjor@Abenezers-MacBook-Pro.local>
2020-03-29 15:08:19 -0500

    displaying total no of cases on client side

--- api.py
+++ api.py
@@ -10,26 +10,19 @@ def home():
     url = 'https://api.covid19api.com/dayone/country/ethiopia/status/confirmed/live'
     country = 'ethiopia'
     r = requests.get(url.format(country)).json()
-    currentNo = []
-    currentNo.append( r[-1:])
+    #currentNo = []
+    #currentNo.append( r[-1:])
     latest = r[-1]
-  #  print(latest)
-   # print(type(latest))
-    print(latest["Cases"])
-
+    CaseNo = latest["Cases"]
+    #print(latest)
+    #print(type(latest))
+    print(CaseNo)
+    
    
     # for i in r :
     #     for key, value in i.items():
     #        print(key, value)
-  
-
-  
-
-
-
-
-    return render_template('index.html')
+    return render_template('index.html', CaseNo = CaseNo)
     
-
 if __name__ == '__main__':
     app.run(debug=True)
