890007beeb096b429e4ab7c4016bc4e8c96f5e92
Abenezer Monjor <abenezermonjor@Abenezers-MacBook-Pro.local>
2020-04-05 15:43:15 -0500

    adding a table for display info per countries

--- api.py
+++ api.py
@@ -7,8 +7,9 @@ app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
-    url = 'https://api.covid19api.com/dayone/country/ethiopia/status/confirmed/live'
-    country = 'ethiopia'
+    country = "ethiopia"
+    url = 'https://api.covid19api.com/dayone/country/'+country+'/status/confirmed/live'
+    
     r = requests.get(url.format(country)).json()
     #currentNo = []
     #currentNo.append( r[-1:])
@@ -26,3 +27,11 @@ def home():
     
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
+
+
+#run the app: FLASK_APP=api.py flask run    
