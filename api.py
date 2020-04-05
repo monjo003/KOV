ed0208ef63e710d2573df54019a9cdcacc32f212
Abenezer Monjor <abenezermonjor@Abenezers-MacBook-Pro.local>
2020-04-05 16:09:43 -0500

    displaying all info per country on client side

--- api.py
+++ api.py
@@ -8,22 +8,25 @@ app.config["DEBUG"] = True
 @app.route('/', methods=['GET'])
 def home():
     country = "ethiopia"
-    url = 'https://api.covid19api.com/dayone/country/'+country+'/status/confirmed/live'
+    url = "https://coronavirus-19-api.herokuapp.com/countries/" + country
     
-    r = requests.get(url.format(country)).json()
-    #currentNo = []
-    #currentNo.append( r[-1:])
-    latest = r[-1]
-    CaseNo = latest["Cases"]
-    #print(latest)
-    #print(type(latest))
-    print(CaseNo)
-    
-   
-    # for i in r :
-    #     for key, value in i.items():
-    #        print(key, value)
-    return render_template('index.html', CaseNo = CaseNo)
+    req_data = requests.get(url.format(country)).json()
+    country = req_data['country']
+    cases  = req_data['cases']
+    todayCases = req_data['todayCases']
+    deaths = req_data['deaths']
+    todayDeaths = req_data['todayDeaths']
+    recovered = req_data['recovered']
+    active = req_data['active']
+    critical = req_data['critical']
+    casesPerOneMillion = req_data['casesPerOneMillion']
+    deathsPerOneMillion = req_data['deathsPerOneMillion']
+    totalTests = req_data['totalTests']
+    testsPerOneMillion = req_data['testsPerOneMillion']
+
+    print(cases)
+
+    return render_template('index.html', country = country, cases = cases, todayCases = todayCases, deaths = deaths, todayDeaths = todayDeaths, recovered = recovered, active = active, critical = critical, casesPerOneMillion = casesPerOneMillion, deathsPerOneMillion = deathsPerOneMillion, totalTests=totalTests, testsPerOneMillion = testsPerOneMillion  )
     
 if __name__ == '__main__':
     app.run(debug=True)
